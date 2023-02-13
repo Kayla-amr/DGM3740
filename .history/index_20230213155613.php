4c350ba019ceb406f90cc48902d260503b683f79
Kayla Macias <kaylamacias.rivera@gmail.com>
2023-02-13 15:57:01 -0700

    breaking into multiple parts

---
+++ .history/index_20230213155613.php
@@ -0,0 +1,109 @@
+<?php
+$title= "Home"
+include '_header.php';
+?>
+            <section class="gallerySlider">
+                <!-- Slider main container -->
+                <div class="swiper">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <div class="swiper-slide">
+                            <picture>
+                                <source media="(min-width: 28.12rem )" srcset="images/galleryPhoto1.jpg">
+                                <source media="(max-width: 28.12rem)" srcset="images/galleryPhoto1_small.jpg">
+                                <img src="images/galleryPhoto1.jpg" alt="Cruise Galler Photos">
+                            </picture>
+                        </div>
+                        <div class="swiper-slide">
+                            <picture>
+                                <source media="(min-width: 28.12rem )" srcset="images/galleryPhoto2.jpg">
+                                <source media="(max-width: 28.12rem)" srcset="images/galleryPhoto2_small.jpg">
+                                <img src="images/galleryPhoto2.jpg" alt="Cruise Galler Photos">
+                            </picture>
+                        </div>
+                        <div class="swiper-slide">
+                            <picture>
+                                <source media="(min-width: 28.12rem )" srcset="images/galleryPhoto3.jpg">
+                                <source media="(max-width: 28.12rem)" srcset="images/galleryPhoto3_small.jpg">
+                                <img src="images/galleryPhoto3.jpg" alt="Cruise Galler Photos">
+                            </picture>
+                        </div>
+                        <div class="swiper-slide">
+                            <picture>
+                                <source media="(min-width: 28.12rem )" srcset="images/galleryPhoto4.jpg">
+                                <source media="(max-width: 28.12rem)" srcset="images/galleryPhoto4_small.jpg">
+                                <img src="images/galleryPhoto4.jpg" alt="Cruise Galler Photos">
+                            </picture>
+                        </div>
+                    </div>
+                    <!-- If we need pagination -->
+                    <div class="swiper-pagination"></div>
+
+            </section>
+            <section class="blogArticles">
+                <h3>Recent Blog Articles</h3>
+                <div class="articles">
+                    <a href="">
+                        <figure>
+                            <img src="images/familyFun.jpg" alt="">
+                            <figcaption>Familiy Fun</figcaption>
+                        </figure>
+                    </a>
+
+                    <a href="">
+                        <figure>
+                            <img src="images/dining.jpg" alt="">
+                            <figcaption>Dining</figcaption>
+                        </figure>
+                    </a>
+
+                    <a href="">
+                        <figure>
+                            <img src="images/snorkeling.jpg" alt="">
+                            <figcaption>Snorkeling</figcaption>
+                        </figure>
+                    </a>
+
+                    <a href="">
+                        <figure>
+                            <img src="images/beach.jpg" alt="">
+                            <figcaption>Beach Relaxation</figcaption>
+                        </figure>
+                    </a>
+
+                </div>
+            </section>
+
+    <?php
+$title= "Home"
+include '_header.php';
+?>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+    <script src="js/scripts.js"></script>
+
+    <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "TravelAgency",
+            "name": "Combat Cruise",
+            "description": "Combat Cruises is a travel agency providing the best cruise deals on our online travel website",
+            "openingHours": "Mo-Fr 09:00-16:00",
+            "telephone": "+18018638888",
+            "faxNumber": "+18018633245",
+            "address":{
+                "@type": "PostalAddress",
+                "streetAddress": "23 South Main St. Suite 16",
+                "addressLocality": "Lexington",
+                "addressRegion": "VA"
+            },
+            "image": "http//fullpath.com/logo1x.png",
+            "priceRange": "$500 to $4000"
+
+        }
+    </script>
+
+</body>
+</html>
---
+++ scss/_footer.php
@@ -0,0 +1,62 @@
+</main>
+    </div>
+
+    <div class="directoryWrapper">
+        <section class="directoryInfo">
+            <div class="contact">
+                <h3>Contact Us</h3>
+                <div class="address">
+                    <p>CABOT CRUIZES <br>
+                        23 South Main St. Suite 16 <br>
+                        Lexington, VA 24450</p>
+                </div>
+
+                <div class="phone">
+                    <h3>Phone:</h3>
+                    <p>1-800-555-1234</p>
+                </div>
+
+                <div class="officeHours">
+                    <h3>Hours of Operation:</h3>
+                    <p>Monday-Friday 9am-4pm</p>
+                </div>
+            </div>
+
+            <div class="newsLetter">
+                <h3>News Letter</h3>
+                <p>Subscribe to our email list and stay up-to-date with our hottest offers and latest specials</p>
+                <input type="email" placeholder="Johndoe@gmail.com">
+                <button>Subscribe</button>
+            </div>
+
+            <div class="moreInfo">
+                <h3>Why Cabot Cruises?</h3>
+                <p>Cabot Cruises is a travel agency providing the best cruise
+                    deals on our online travel website. We can help you with all
+                    inclusive vacaMons including discounted cruises.
+                    <br>
+                    We do not sell travel to residents of Delaware, Iowa,
+                    Florida, Hawaii and Washington state because those states
+                    are just wierd. If you are a resident of one of these states,
+                    call your congressman and tell them to change the
+                    regulaMons.
+                </p>
+            </div>
+        </section>
+    </div>
+
+    <div class="mediaWrapper">
+        <section class="socialMedia">
+            <ion-icon name="logo-facebook"></ion-icon>
+            <ion-icon name="logo-youtube"></ion-icon>
+            <ion-icon name="logo-instagram"></ion-icon>
+            <ion-icon name="logo-pinterest"></ion-icon>
+            <ion-icon name="logo-twitter"></ion-icon>
+        </section>
+    </div>
+
+    <div class="footerWrapper">
+        <footer>
+            <p>&copy; 2023 Kayla Macias &bull; <a href="https://kaylamaciasrivera.netlify.app/">My Portfolio</a></p>
+        </footer>
+    </div>
