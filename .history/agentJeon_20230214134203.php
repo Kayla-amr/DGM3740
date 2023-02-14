fa838ef985cd0f873d3a8f5156b8cdc653636956
Kayla Macias <kaylamacias.rivera@gmail.com>
2023-02-14 13:42:35 -0700

    adding break points

--- .history/agentJeon_20230214134203.php
+++ .history/agentJeon_20230214134203.php
@@ -1,30 +1,29 @@
 <?php
-$title= "Agent - Kim Taehyung";
+$title= "Agent - Jeon Jungkook";
 include '_header.php';
 ?>
 
-<h1 class="agentName">Agent - Kim Taehyung</h1>
+<h1 class="agentName">Agent - Jeon Jungkook</h1>
 <div></div>
 <div class="agentInfo">
-    <img src="/images/agentk.png" alt="">
-<h2>Phone:</h2>
-    <p>1-800-555-1234 ext. 12</p>
+    <img src="/images/agentj.png" alt="">
+    <h2>Phone:</h2>
+    <p>1-800-555-1234 ext. 07</p>
 
     <h2>Working:</h2>
-    <p>Monday - Friday</p>
+    <p>Monday, Wednesday, Friday</p>
 
     <h2>Destination Specialization:</h2>
     <p>Israel, Egypt, Italy, Greece, Hawaii, Guatemala, Southern California, Orlando, Las Vegas, Bahamas,
     Caribbean cruises.</p>
 
     <h2>Office Hours:</h2>
-    <p>7:00am-5:00pm</p>
+    <p>8:00am-6:00pm</p>
 
     <h2>Biography:</h2>
     <p>I have loved travel for as long as I can remember. At the age of 12 I asked if I could fly by myself
     to Hawaii and I never came back.
-    <br>
-    <br>
+    <br> 
     Right out of high school I started into travel school and have had the opportunity to work in many aspects of the industry. In every aspect I have
     worked I have had the opportunity to help people travel, to help fulfill people?s dreams, what could be be6er to do every day!</p>
 
@@ -37,7 +36,6 @@ include '_header.php';
         <li>"Thanks for your pa`ence and efficiency. Let's do it again some`me very soon!"
         <br>-Jane and John Westagar</li>
     </ul>
-
 </div>
 
 <?php
--- .history/agentKim_20230214134218.php
+++ .history/agentKim_20230214134218.php
@@ -24,7 +24,7 @@ include '_header.php';
     <p>I have loved travel for as long as I can remember. At the age of 12 I asked if I could fly by myself
     to Hawaii and I never came back.
     <br>
-    <br>
+    
     Right out of high school I started into travel school and have had the opportunity to work in many aspects of the industry. In every aspect I have
     worked I have had the opportunity to help people travel, to help fulfill people?s dreams, what could be be6er to do every day!</p>
 
