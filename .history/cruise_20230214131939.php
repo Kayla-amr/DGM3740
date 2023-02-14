db06143029a80f848da787d964b8f9035c92a42c
Kayla Macias <kaylamacias.rivera@gmail.com>
2023-02-14 13:20:51 -0700

    adjusting css

--- .history/cruise_20230214131939.php
+++ .history/cruise_20230214131939.php
@@ -5,7 +5,7 @@ include '_header.php';
 <h1 class="cruiseTitle">Cruises</h1>
 
 <div class="cruiseInfo">
-    <div class="optionInfo">
+    <div class="i">
         <h2>Australia & New Zealand Cruises from $999!</h2>
         <p>On the other side of the world, Australia and New Zealand are full of wonders to behold! Now, more than ever before, you can get
         phenomenal discounts to cruise the South Pacific. Sail to New Zealand from Sydney, Australia on a 12-day cruise from just $888 per
@@ -15,7 +15,7 @@ include '_header.php';
         </div>
     </div>
 
-    <div class="optionInfo">
+    <div>
         <h2>Hawaii & Mexico Cruises from $899!</h2>
         <p>On the other side of the world, Australia and New Zealand are full of wonders to behold! Now, more than ever before, you can get
         phenomenal discounts to cruise the South Pacific. Sail to New Zealand from Sydney, Australia on a 12-day cruise from just $888 per
@@ -26,7 +26,7 @@ include '_header.php';
         
     </div>
 
-    <div class="optionInfo">
+    <div>
         <h2>Florida & Cabo Cruises from $900!</h2>
         <p>On the other side of the world, Australia and New Zealand are full of wonders to behold! Now, more than ever before, you can get
         phenomenal discounts to cruise the South Pacific. Sail to New Zealand from Sydney, Australia on a 12-day cruise from just $888 per
@@ -37,7 +37,7 @@ include '_header.php';
         </div>
     </div>
 
-    <div class="optionInfo">
+    <div>
         <h2>Australia & New Zealand Cruises from $1099!</h2>
         <p>On the other side of the world, Australia and New Zealand are full of wonders to behold! Now, more than ever before, you can get
         phenomenal discounts to cruise the South Pacific. Sail to New Zealand from Sydney, Australia on a 12-day cruise from just $888 per
