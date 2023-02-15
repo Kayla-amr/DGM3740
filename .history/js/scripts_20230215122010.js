95bfed7a6a09102700338dc6d7b8a4c2654d981e
Kayla Macias <kaylamacias.rivera@gmail.com>
2023-02-15 12:20:19 -0700

    adjusting js

---
+++ .history/js/scripts_20230215122010.js
@@ -0,0 +1,58 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed:2000,
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    //Enable Auto Play
+    autoplay:{
+        delay:5000,
+    },
+
+  });
+
+  function toggleMenu() {
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+var x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu; 
+
+
+///////Assignment 2
+var pageLocation = location.href;
+console.log(pageLocation);
+
+var splitLink = pageLocation.split('/');
+console.log(splitLink);
+
+var numberOfParts = splitLink.length;
+console.log(numberOfParts);
+
+var pieceIwant = splitLink[numberOfParts - 1];
+console.log('>>>>>> '+pieceIwant)
+
+var countUl = document.querySelectorAll("ul#primaryNav li a")
+console.log(countUl)
+
+
+var test3 = "index.php";
+
+var i;
+for(i = 0; i < countUl.length; i++){
+  var myPage = countUl[0].getAttribute("href").split('/');
+  console.log(myPage);
+
+  if(test3 === myPage){
+    test3[i].parentNode.className = "active";
+    test3[i].parentNode.parentNode.parentNode.className = "parent";
+  } 
+  else{
+    test3[i].parentNode.className ="";
+  }
+ }
